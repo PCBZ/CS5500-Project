d1c0de388446a2691adab667930eeab623777159
Jaybeey1507 <akanbijubriladeyemi@outlook.com>
2025-03-27 21:38:59 -0700

    Updated App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import Login from './components/auth/Login.jsx';
 import Register from './components/auth/Register.jsx';
 import Dashboard from './components/Dashboard.jsx';
 import EventManagement from './components/events/EventManagement.jsx';
+import CreateNewEvent from './components/events/CreateNewEvent.jsx';
 import Donors from './components/donors/Donors.jsx';
 import Navbar from './components/common/Navbar.jsx';
 import ProtectedRoute from './components/auth/ProtectedRoute.jsx';
@@ -21,7 +22,10 @@ function App() {
             <Route path="/login" component={Login} />
             <Route path="/register" component={Register} />
             <ProtectedRoute path="/dashboard" component={Dashboard} />
-            <Route path="/events" component={EventManagement} />
+            {/* Add a specific route for creating a new event */}
+            <ProtectedRoute path="/events/create" component={CreateNewEvent} />
+            {/* Ensure the /events route comes after the more specific /events/create */}
+            <ProtectedRoute path="/events" component={EventManagement} />
             <ProtectedRoute path="/donors" component={Donors} />
             <Redirect from="/" to="/login" />
           </Switch>
@@ -31,4 +35,4 @@ function App() {
   );
 }
 
-export default App; 
+export default App;
