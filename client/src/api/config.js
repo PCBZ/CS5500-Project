ebf64e45e4271682afc34eb04f94b51bcfe56986
PCBZ <devil02047@outlook.com>
2025-04-18 16:20:24 -0700

    set CDc API

---
+++ client/src/api/config.js
@@ -0,0 +1,4 @@
+const API_URL = process.env.REACT_APP_API_URL || 'https://cs5500-project.onrender.com';
+const BASE_URL = process.env.REACT_APP_BASE_URL || 'https://pcbz.github.io/CS5500-Project';
+
+export { API_URL, BASE_URL }; 
